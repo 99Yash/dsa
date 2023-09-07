59a0b1b207c85942c9faccd4b70681675530c5d5
yash <yashgouravkar@gmail.com>
2023-09-07 18:02:25 +0530

    init

---
+++ index.ts
@@ -0,0 +1,10 @@
+function addTo(n: number) {
+  // let total = 0;
+  // for (let i = 1; i <= n; i++) {
+  //   total += i;
+  // }
+  // return total;
+  return (n * (n + 1)) / 2;
+}
+
+console.log(addTo(29));
